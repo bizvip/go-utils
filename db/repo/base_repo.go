09827161c6cc3b9360061cb2b3a6421065b43fda
azhang <azhang@whitesand.online>
2024-05-23 15:53:23 +0700

    init

--- db/repo/base_repo.go
+++ db/repo/base_repo.go
@@ -147,6 +147,9 @@ func WithOrderBy(orderBy string) SelOpt {
 func WithLimit(limit int) SelOpt {
 	return func(q *gorm.DB) *gorm.DB { return q.Limit(limit) }
 }
+func WithWhere(condition map[string]interface{}) SelOpt {
+	return func(q *gorm.DB) *gorm.DB { return q.Where(condition) }
+}
 
 // SelectBy 按照条件查找多条 可使用链式方法添加order和limit等参数
 func (r *BaseRepo[T]) SelectBy(condition map[string]interface{}, results *[]*T, opts ...SelOpt) error {
