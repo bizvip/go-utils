978482d29eb9e459e0aeba45ddb04af9c3605fcd
az <asdfdsa@fdsafsda.com>
2024-06-17 11:14:06 +0700

    update crypto.go

--- crypto.go
+++ crypto.go
@@ -22,6 +22,7 @@ func NewCryptoUtils() *CryptoUtils {
 	return &CryptoUtils{}
 }
 
+// Encrypt 微调
 func (u *CryptoUtils) Encrypt(text string, pass string) (string, error) {
 	salt := make([]byte, 16)
 	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
@@ -46,22 +47,93 @@ func (u *CryptoUtils) Encrypt(text string, pass string) (string, error) {
 		return "", err
 	}
 
-	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
-	ciphertext = append(salt, ciphertext...)
+	ciphertext := aesGCM.Seal(nil, nonce, plaintext, nil)
+	ciphertext = append(salt, append(nonce, ciphertext...)...)
+
 	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
 
-func (u *CryptoUtils) Decrypt(text string, pass string) (string, error) {
-	enc, err := base64.StdEncoding.DecodeString(text)
+// func (u *CryptoUtils) Encrypt(text string, pass string) (string, error) {
+// 	salt := make([]byte, 16)
+// 	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
+// 		return "", err
+// 	}
+// 	key := pbkdf2.Key([]byte(pass), salt, 10000, 32, sha3.New512)
+//
+// 	plaintext := []byte(text)
+//
+// 	block, err := aes.NewCipher(key)
+// 	if err != nil {
+// 		return "", err
+// 	}
+//
+// 	aesGCM, err := cipher.NewGCM(block)
+// 	if err != nil {
+// 		return "", err
+// 	}
+//
+// 	nonce := make([]byte, aesGCM.NonceSize())
+// 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+// 		return "", err
+// 	}
+//
+// 	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
+// 	ciphertext = append(salt, ciphertext...)
+// 	return base64.StdEncoding.EncodeToString(ciphertext), nil
+// }
+
+// func (u *CryptoUtils) Decrypt(text string, pass string) (string, error) {
+// 	enc, err := base64.StdEncoding.DecodeString(text)
+// 	if err != nil {
+// 		return "", err
+// 	}
+//
+// 	if len(enc) < 16 {
+// 		return "", fmt.Errorf("文本不合法")
+// 	}
+// 	salt := enc[:16]
+// 	enc = enc[16:]
+//
+// 	key := pbkdf2.Key([]byte(pass), salt, 10000, 32, sha3.New512)
+//
+// 	block, err := aes.NewCipher(key)
+// 	if err != nil {
+// 		return "", err
+// 	}
+//
+// 	aesGCM, err := cipher.NewGCM(block)
+// 	if err != nil {
+// 		return "", err
+// 	}
+//
+// 	nonceSize := aesGCM.NonceSize()
+// 	if len(enc) < nonceSize {
+// 		return "", fmt.Errorf("文本不合法")
+// 	}
+// 	nonce, cipherBytes := enc[:nonceSize], enc[nonceSize:]
+//
+// 	plaintext, err := aesGCM.Open(nil, nonce, cipherBytes, nil)
+// 	if err != nil {
+// 		return "", err
+// 	}
+//
+// 	return string(plaintext), nil
+// }
+
+// Decrypt 微调整
+func (u *CryptoUtils) Decrypt(cipherText string, pass string) (string, error) {
+	enc, err := base64.StdEncoding.DecodeString(cipherText)
 	if err != nil {
 		return "", err
 	}
 
-	if len(enc) < 16 {
-		return "", fmt.Errorf("文本不合法")
+	if len(enc) < 28 {
+		return "", fmt.Errorf("text too short")
 	}
+
 	salt := enc[:16]
-	enc = enc[16:]
+	nonce := enc[16:28]
+	cipherBytes := enc[28:]
 
 	key := pbkdf2.Key([]byte(pass), salt, 10000, 32, sha3.New512)
 
@@ -74,12 +146,6 @@ func (u *CryptoUtils) Decrypt(text string, pass string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
-	nonceSize := aesGCM.NonceSize()
-	if len(enc) < nonceSize {
-		return "", fmt.Errorf("文本不合法")
-	}
-	nonce, cipherBytes := enc[:nonceSize], enc[nonceSize:]
 
 	plaintext, err := aesGCM.Open(nil, nonce, cipherBytes, nil)
 	if err != nil {
