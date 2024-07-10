94ead5cc6aebc59a4f98c46d705088ca8761cd1d
az <asdfdsa@fdsafsda.com>
2024-07-11 03:36:22 +0700

    updated str tools

--- v2/pwd/pwd.go
+++ v2/pwd/pwd.go
@@ -3,17 +3,40 @@
  * Author ORCID: https://orcid.org/0009-0003-8150-367X                        *
  ******************************************************************************/
 
-package gopwd
+package pwd
 
 import (
 	"crypto/rand"
 	"crypto/subtle"
 	"encoding/base64"
+	"errors"
 	"fmt"
+	"regexp"
 	"strings"
 
 	"golang.org/x/crypto/argon2"
 )
+
+// 定义常见错误消息
+var (
+	ErrInvalidSecPwdLength      = errors.New("security password must be 6 digits")
+	ErrInvalidSecPwdConsecutive = errors.New("security password must not contain three consecutive numbers")
+)
+
+// ValidateSecPwd 验证交易密码
+func ValidateSecPwd(secPwd string) error {
+	matched, err := regexp.MatchString(`^[0-9]{6}$`, secPwd)
+	if err != nil || !matched {
+		return ErrInvalidSecPwdLength
+	}
+	// 禁止三个连续数字
+	for i := 0; i < len(secPwd)-2; i++ {
+		if secPwd[i+1] == secPwd[i]+1 && secPwd[i+2] == secPwd[i]+2 {
+			return ErrInvalidSecPwdConsecutive
+		}
+	}
+	return nil
+}
 
 // StrToPwd 使用 argon2id 算法生成密码的散列值
 func StrToPwd(password string) (string, error) {
