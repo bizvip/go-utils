89dda6f9e8e4a66b962351f68066e197c9f5d706
az <asdfdsa@fdsafsda.com>
2024-07-11 10:52:37 +0700

    updated str tools

--- v2/pwd/pwd.go
+++ v2/pwd/pwd.go
@@ -19,22 +19,37 @@ import (
 
 // 定义常见错误消息
 var (
-	ErrInvalidSecPwdLength      = errors.New("security password must be 6 digits")
-	ErrInvalidSecPwdConsecutive = errors.New("security password must not contain three consecutive numbers")
+	ErrInvalidSecPwdLength      = errors.New("password must be 6 digits")
+	ErrInvalidSecPwdConsecutive = errors.New("password must not contain three consecutive identical digits")
+	ErrInvalidSecPwdSequential  = errors.New("password must not contain three or more sequential digits")
 )
 
-// ValidateSecPwd 验证交易密码
-func ValidateSecPwd(secPwd string) error {
-	matched, err := regexp.MatchString(`^[0-9]{6}$`, secPwd)
+// NumberPwdValidate 验证指定长度的纯数字字符串密码
+func NumberPwdValidate(secPwd string, length int) error {
+	// 构建正则表达式以匹配指定长度的数字字符串
+	regexPattern := fmt.Sprintf(`^[0-9]{%d}$`, length)
+	matched, err := regexp.MatchString(regexPattern, secPwd)
 	if err != nil || !matched {
 		return ErrInvalidSecPwdLength
 	}
-	// 禁止三个连续数字
+
+	// 检查是否包含三个或更多连续相同的数字
 	for i := 0; i < len(secPwd)-2; i++ {
-		if secPwd[i+1] == secPwd[i]+1 && secPwd[i+2] == secPwd[i]+2 {
+		if secPwd[i] == secPwd[i+1] && secPwd[i] == secPwd[i+2] {
 			return ErrInvalidSecPwdConsecutive
 		}
 	}
+
+	// 检查是否为三个或更多递增或递减的顺子
+	for i := 0; i < len(secPwd)-2; i++ {
+		if secPwd[i+1] == secPwd[i]+1 && secPwd[i+2] == secPwd[i]+2 {
+			return ErrInvalidSecPwdSequential
+		}
+		if secPwd[i+1] == secPwd[i]-1 && secPwd[i+2] == secPwd[i]-2 {
+			return ErrInvalidSecPwdSequential
+		}
+	}
+
 	return nil
 }
 
