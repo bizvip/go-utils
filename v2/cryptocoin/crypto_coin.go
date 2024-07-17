91daa099be61afb07147b62bfdb03eb4b5c4989a
az <asdfdsa@fdsafsda.com>
2024-07-17 13:35:14 +0700

    updated utils to v2

--- v2/cryptocoin/crypto_coin.go
+++ v2/cryptocoin/crypto_coin.go
@@ -11,8 +11,19 @@ import (
 	"strings"
 )
 
-func IsValidErc20Address(address string) bool {
+// IsValidERC20Address erc20地址字符串合法性校验
+func IsValidERC20Address(address string) bool {
 	return len(address) == 42 && strings.HasPrefix(address, "0x")
+}
+
+// IsValidTRC20Address trc20地址字符串合法性校验
+func IsValidTRC20Address(address string) bool {
+	return len(address) == 34 && strings.HasPrefix(address, "T")
+}
+
+// IsValidWtcAddress wtc地址字符串合法性校验
+func IsValidWtcAddress(address string) bool {
+	return len(address) == 42 && strings.HasPrefix(address, "wtc")
 }
 
 // GenWBAddress 生成wtc币的收款地址
