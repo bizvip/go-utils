d437b1607e1383ecf0b169953d6cfc350e6fc0e5
geek_inside <38232156+bizvip@users.noreply.github.com>
2024-08-19 00:55:41 +0700

    update snowflake.go

--- snowflake/snowflake.go
+++ snowflake/snowflake.go
@@ -1,379 +1,229 @@
-/******************************************************************************
- * Copyright (c) 2024. Archer++. All rights reserved.                         *
- * Author ORCID: https://orcid.org/0009-0003-8150-367X                        *
- ******************************************************************************/
-
-// Package snowflake provides a very simple Twitter snowflake generator and parser.
-package snowflake
+// Package sonyflake implements Sonyflake, a distributed unique ID generator inspired by Twitter's Snowflake.
+//
+// A Sonyflake ID is composed of
+//
+//	39 bits for time in units of 10 msec
+//	 8 bits for a sequence number
+//	16 bits for a machine id
+package sonyflake
 
 import (
-	"encoding/base64"
-	"encoding/binary"
 	"errors"
-	"fmt"
-	"strconv"
+	"net"
 	"sync"
 	"time"
 )
 
-var (
-	// Epoch is set to the twitter snowflake epoch of Nov 04 2010 01:42:54 UTC in milliseconds
-	// You may customize this to set a different epoch for your application.
-	Epoch int64 = 1288834974657
+type InterfaceAddrs func() ([]net.Addr, error)
 
-	// NodeBits holds the number of bits to use for Node
-	// Remember, you have a total 22 bits to share between Node/Step
-	NodeBits uint8 = 10
-
-	// StepBits holds the number of bits to use for Step
-	// Remember, you have a total 22 bits to share between Node/Step
-	StepBits uint8 = 12
-
-	// DEPRECATED: the below four variables will be removed in a future release.
-	mu        sync.Mutex
-	nodeMax   int64 = -1 ^ (-1 << NodeBits)
-	nodeMask        = nodeMax << StepBits
-	stepMask  int64 = -1 ^ (-1 << StepBits)
-	timeShift       = NodeBits + StepBits
-	nodeShift       = StepBits
+// These constants are the bit lengths of Sonyflake ID parts.
+const (
+	BitLenTime      = 39                               // bit length of time
+	BitLenSequence  = 8                                // bit length of sequence number
+	BitLenMachineID = 63 - BitLenTime - BitLenSequence // bit length of machine id
 )
 
-const encodeBase32Map = "ybndrfg8ejkmcpqxot1uwisza345h769"
-
-var decodeBase32Map [256]byte
-
-const encodeBase58Map = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
-
-var decodeBase58Map [256]byte
-
-// A JSONSyntaxError is returned from UnmarshalJSON if an invalid ID is provided.
-type JSONSyntaxError struct{ original []byte }
-
-func (j JSONSyntaxError) Error() string {
-	return fmt.Sprintf("invalid snowflake ID %q", string(j.original))
+// Settings configures Sonyflake:
+//
+// StartTime is the time since which the Sonyflake time is defined as the elapsed time.
+// If StartTime is 0, the start time of the Sonyflake is set to "2014-09-01 00:00:00 +0000 UTC".
+// If StartTime is ahead of the current time, Sonyflake is not created.
+//
+// MachineID returns the unique ID of the Sonyflake instance.
+// If MachineID returns an error, Sonyflake is not created.
+// If MachineID is nil, default MachineID is used.
+// Default MachineID returns the lower 16 bits of the private IP address.
+//
+// CheckMachineID validates the uniqueness of the machine ID.
+// If CheckMachineID returns false, Sonyflake is not created.
+// If CheckMachineID is nil, no validation is done.
+type Settings struct {
+	StartTime      time.Time
+	MachineID      func() (uint16, error)
+	CheckMachineID func(uint16) bool
 }
 
-// ErrInvalidBase58 is returned by ParseBase58 when given an invalid []byte
-var ErrInvalidBase58 = errors.New("invalid base58")
-
-// ErrInvalidBase32 is returned by ParseBase32 when given an invalid []byte
-var ErrInvalidBase32 = errors.New("invalid base32")
-
-// Create maps for decoding Base58/Base32.
-// This speeds up the process tremendously.
-func init() {
-
-	for i := 0; i < len(decodeBase58Map); i++ {
-		decodeBase58Map[i] = 0xFF
-	}
-
-	for i := 0; i < len(encodeBase58Map); i++ {
-		decodeBase58Map[encodeBase58Map[i]] = byte(i)
-	}
-
-	for i := 0; i < len(decodeBase32Map); i++ {
-		decodeBase32Map[i] = 0xFF
-	}
-
-	for i := 0; i < len(encodeBase32Map); i++ {
-		decodeBase32Map[encodeBase32Map[i]] = byte(i)
-	}
+// Sonyflake is a distributed unique ID generator.
+type Sonyflake struct {
+	mutex       *sync.Mutex
+	startTime   int64
+	elapsedTime int64
+	sequence    uint16
+	machineID   uint16
 }
 
-// A Node struct holds the basic information needed for a snowflake generator
-// node
-type Node struct {
-	mu    sync.Mutex
-	epoch time.Time
-	time  int64
-	node  int64
-	step  int64
+var (
+	ErrStartTimeAhead   = errors.New("start time is ahead of now")
+	ErrNoPrivateAddress = errors.New("no private ip address")
+	ErrOverTimeLimit    = errors.New("over the time limit")
+	ErrInvalidMachineID = errors.New("invalid machine id")
+)
 
-	nodeMax   int64
-	nodeMask  int64
-	stepMask  int64
-	timeShift uint8
-	nodeShift uint8
-}
+var defaultInterfaceAddrs = net.InterfaceAddrs
 
-// An ID is a custom type used for a snowflake ID.  This is used so we can
-// attach methods onto the ID.
-type ID int64
-
-// NewNode returns a new snowflake node that can be used to generate snowflake
-// IDs
-func NewNode(node int64) (*Node, error) {
-
-	if NodeBits+StepBits > 22 {
-		return nil, errors.New("Remember, you have a total 22 bits to share between Node/Step")
-	}
-	// re-calc in case custom NodeBits or StepBits were set
-	// DEPRECATED: the below block will be removed in a future release.
-	mu.Lock()
-	nodeMax = -1 ^ (-1 << NodeBits)
-	nodeMask = nodeMax << StepBits
-	stepMask = -1 ^ (-1 << StepBits)
-	timeShift = NodeBits + StepBits
-	nodeShift = StepBits
-	mu.Unlock()
-
-	n := Node{}
-	n.node = node
-	n.nodeMax = -1 ^ (-1 << NodeBits)
-	n.nodeMask = n.nodeMax << StepBits
-	n.stepMask = -1 ^ (-1 << StepBits)
-	n.timeShift = NodeBits + StepBits
-	n.nodeShift = StepBits
-
-	if n.node < 0 || n.node > n.nodeMax {
-		return nil, errors.New("Node number must be between 0 and " + strconv.FormatInt(n.nodeMax, 10))
+// New returns a new Sonyflake configured with the given Settings.
+// New returns an error in the following cases:
+// - Settings.StartTime is ahead of the current time.
+// - Settings.MachineID returns an error.
+// - Settings.CheckMachineID returns false.
+func New(st Settings) (*Sonyflake, error) {
+	if st.StartTime.After(time.Now()) {
+		return nil, ErrStartTimeAhead
 	}
 
-	var curTime = time.Now()
-	// add time.Duration to curTime to make sure we use the monotonic clock if available
-	n.epoch = curTime.Add(time.Unix(Epoch/1000, (Epoch%1000)*1000000).Sub(curTime))
+	sf := new(Sonyflake)
+	sf.mutex = new(sync.Mutex)
+	sf.sequence = uint16(1<<BitLenSequence - 1)
 
-	return &n, nil
-}
-
-// Generate creates and returns a unique snowflake ID
-// To help guarantee uniqueness
-// - Make sure your system is keeping accurate system time
-// - Make sure you never have multiple nodes running with the same node ID
-func (n *Node) Generate() ID {
-
-	n.mu.Lock()
-	defer n.mu.Unlock()
-
-	now := time.Since(n.epoch).Milliseconds()
-
-	if now == n.time {
-		n.step = (n.step + 1) & n.stepMask
-
-		if n.step == 0 {
-			for now <= n.time {
-				now = time.Since(n.epoch).Milliseconds()
-			}
-		}
+	if st.StartTime.IsZero() {
+		sf.startTime = toSonyflakeTime(time.Date(2014, 9, 1, 0, 0, 0, 0, time.UTC))
 	} else {
-		n.step = 0
+		sf.startTime = toSonyflakeTime(st.StartTime)
 	}
 
-	n.time = now
-
-	r := ID((now)<<n.timeShift |
-		(n.node << n.nodeShift) |
-		(n.step),
-	)
-
-	return r
-}
-
-// Int64 returns an int64 of the snowflake ID
-func (f ID) Int64() int64 {
-	return int64(f)
-}
-
-// ParseInt64 converts an int64 into a snowflake ID
-func ParseInt64(id int64) ID {
-	return ID(id)
-}
-
-// String returns a string of the snowflake ID
-func (f ID) String() string {
-	return strconv.FormatInt(int64(f), 10)
-}
-
-// ParseString converts a string into a snowflake ID
-func ParseString(id string) (ID, error) {
-	i, err := strconv.ParseInt(id, 10, 64)
-	return ID(i), err
-
-}
-
-// Base2 returns a string base2 of the snowflake ID
-func (f ID) Base2() string {
-	return strconv.FormatInt(int64(f), 2)
-}
-
-// ParseBase2 converts a Base2 string into a snowflake ID
-func ParseBase2(id string) (ID, error) {
-	i, err := strconv.ParseInt(id, 2, 64)
-	return ID(i), err
-}
-
-// Base32 uses the z-base-32 character set but encodes and decodes similar
-// to base58, allowing it to create an even smaller result string.
-// NOTE: There are many different base32 implementations so becareful when
-// doing any interoperation.
-func (f ID) Base32() string {
-
-	if f < 32 {
-		return string(encodeBase32Map[f])
+	var err error
+	if st.MachineID == nil {
+		sf.machineID, err = lower16BitPrivateIP(defaultInterfaceAddrs)
+	} else {
+		sf.machineID, err = st.MachineID()
 	}
-
-	b := make([]byte, 0, 12)
-	for f >= 32 {
-		b = append(b, encodeBase32Map[f%32])
-		f /= 32
-	}
-	b = append(b, encodeBase32Map[f])
-
-	for x, y := 0, len(b)-1; x < y; x, y = x+1, y-1 {
-		b[x], b[y] = b[y], b[x]
-	}
-
-	return string(b)
-}
-
-// ParseBase32 parses a base32 []byte into a snowflake ID
-// NOTE: There are many different base32 implementations so becareful when
-// doing any interoperation.
-func ParseBase32(b []byte) (ID, error) {
-
-	var id int64
-
-	for i := range b {
-		if decodeBase32Map[b[i]] == 0xFF {
-			return -1, ErrInvalidBase32
-		}
-		id = id*32 + int64(decodeBase32Map[b[i]])
-	}
-
-	return ID(id), nil
-}
-
-// Base36 returns a base36 string of the snowflake ID
-func (f ID) Base36() string {
-	return strconv.FormatInt(int64(f), 36)
-}
-
-// ParseBase36 converts a Base36 string into a snowflake ID
-func ParseBase36(id string) (ID, error) {
-	i, err := strconv.ParseInt(id, 36, 64)
-	return ID(i), err
-}
-
-// Base58 returns a base58 string of the snowflake ID
-func (f ID) Base58() string {
-
-	if f < 58 {
-		return string(encodeBase58Map[f])
-	}
-
-	b := make([]byte, 0, 11)
-	for f >= 58 {
-		b = append(b, encodeBase58Map[f%58])
-		f /= 58
-	}
-	b = append(b, encodeBase58Map[f])
-
-	for x, y := 0, len(b)-1; x < y; x, y = x+1, y-1 {
-		b[x], b[y] = b[y], b[x]
-	}
-
-	return string(b)
-}
-
-// ParseBase58 parses a base58 []byte into a snowflake ID
-func ParseBase58(b []byte) (ID, error) {
-
-	var id int64
-
-	for i := range b {
-		if decodeBase58Map[b[i]] == 0xFF {
-			return -1, ErrInvalidBase58
-		}
-		id = id*58 + int64(decodeBase58Map[b[i]])
-	}
-
-	return ID(id), nil
-}
-
-// Base64 returns a base64 string of the snowflake ID
-func (f ID) Base64() string {
-	return base64.StdEncoding.EncodeToString(f.Bytes())
-}
-
-// ParseBase64 converts a base64 string into a snowflake ID
-func ParseBase64(id string) (ID, error) {
-	b, err := base64.StdEncoding.DecodeString(id)
 	if err != nil {
-		return -1, err
-	}
-	return ParseBytes(b)
-
-}
-
-// Bytes returns a byte slice of the snowflake ID
-func (f ID) Bytes() []byte {
-	return []byte(f.String())
-}
-
-// ParseBytes converts a byte slice into a snowflake ID
-func ParseBytes(id []byte) (ID, error) {
-	i, err := strconv.ParseInt(string(id), 10, 64)
-	return ID(i), err
-}
-
-// IntBytes returns an array of bytes of the snowflake ID, encoded as a
-// big endian integer.
-func (f ID) IntBytes() [8]byte {
-	var b [8]byte
-	binary.BigEndian.PutUint64(b[:], uint64(f))
-	return b
-}
-
-// ParseIntBytes converts an array of bytes encoded as big endian integer as
-// a snowflake ID
-func ParseIntBytes(id [8]byte) ID {
-	return ID(int64(binary.BigEndian.Uint64(id[:])))
-}
-
-// Time returns an int64 unix timestamp in milliseconds of the snowflake ID time
-// DEPRECATED: the below function will be removed in a future release.
-func (f ID) Time() int64 {
-	return (int64(f) >> timeShift) + Epoch
-}
-
-// Node returns an int64 of the snowflake ID node number
-// DEPRECATED: the below function will be removed in a future release.
-func (f ID) Node() int64 {
-	return int64(f) & nodeMask >> nodeShift
-}
-
-// Step returns an int64 of the snowflake step (or sequence) number
-// DEPRECATED: the below function will be removed in a future release.
-func (f ID) Step() int64 {
-	return int64(f) & stepMask
-}
-
-// MarshalJSON returns a json byte array string of the snowflake ID.
-func (f ID) MarshalJSON() ([]byte, error) {
-	buff := make([]byte, 0, 22)
-	buff = append(buff, '"')
-	buff = strconv.AppendInt(buff, int64(f), 10)
-	buff = append(buff, '"')
-	return buff, nil
-}
-
-// UnmarshalJSON converts a json byte array of a snowflake ID into an ID type.
-func (f *ID) UnmarshalJSON(b []byte) error {
-	if len(b) < 3 || b[0] != '"' || b[len(b)-1] != '"' {
-		return JSONSyntaxError{b}
+		return nil, err
 	}
 
-	i, err := strconv.ParseInt(string(b[1:len(b)-1]), 10, 64)
+	if st.CheckMachineID != nil && !st.CheckMachineID(sf.machineID) {
+		return nil, ErrInvalidMachineID
+	}
+
+	return sf, nil
+}
+
+// NewSonyflake returns a new Sonyflake configured with the given Settings.
+// NewSonyflake returns nil in the following cases:
+// - Settings.StartTime is ahead of the current time.
+// - Settings.MachineID returns an error.
+// - Settings.CheckMachineID returns false.
+func NewSonyflake(st Settings) *Sonyflake {
+	sf, _ := New(st)
+	return sf
+}
+
+// NextID generates a next unique ID.
+// After the Sonyflake time overflows, NextID returns an error.
+func (sf *Sonyflake) NextID() (uint64, error) {
+	const maskSequence = uint16(1<<BitLenSequence - 1)
+
+	sf.mutex.Lock()
+	defer sf.mutex.Unlock()
+
+	current := currentElapsedTime(sf.startTime)
+	if sf.elapsedTime < current {
+		sf.elapsedTime = current
+		sf.sequence = 0
+	} else { // sf.elapsedTime >= current
+		sf.sequence = (sf.sequence + 1) & maskSequence
+		if sf.sequence == 0 {
+			sf.elapsedTime++
+			overtime := sf.elapsedTime - current
+			time.Sleep(sleepTime((overtime)))
+		}
+	}
+
+	return sf.toID()
+}
+
+const sonyflakeTimeUnit = 1e7 // nsec, i.e. 10 msec
+
+func toSonyflakeTime(t time.Time) int64 {
+	return t.UTC().UnixNano() / sonyflakeTimeUnit
+}
+
+func currentElapsedTime(startTime int64) int64 {
+	return toSonyflakeTime(time.Now()) - startTime
+}
+
+func sleepTime(overtime int64) time.Duration {
+	return time.Duration(overtime*sonyflakeTimeUnit) -
+		time.Duration(time.Now().UTC().UnixNano()%sonyflakeTimeUnit)
+}
+
+func (sf *Sonyflake) toID() (uint64, error) {
+	if sf.elapsedTime >= 1<<BitLenTime {
+		return 0, ErrOverTimeLimit
+	}
+
+	return uint64(sf.elapsedTime)<<(BitLenSequence+BitLenMachineID) |
+		uint64(sf.sequence)<<BitLenMachineID |
+		uint64(sf.machineID), nil
+}
+
+func privateIPv4(interfaceAddrs InterfaceAddrs) (net.IP, error) {
+	as, err := interfaceAddrs()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	*f = ID(i)
-	return nil
+	for _, a := range as {
+		ipnet, ok := a.(*net.IPNet)
+		if !ok || ipnet.IP.IsLoopback() {
+			continue
+		}
+
+		ip := ipnet.IP.To4()
+		if isPrivateIPv4(ip) {
+			return ip, nil
+		}
+	}
+	return nil, ErrNoPrivateAddress
 }
 
-// QuickGen 快捷方法直接出uint64方便用到id上
-func QuickGen(nodeId int64) uint64 {
-	node, _ := NewNode(nodeId)
-	return uint64(node.Generate().Int64())
+func isPrivateIPv4(ip net.IP) bool {
+	// Allow private IP addresses (RFC1918) and link-local addresses (RFC3927)
+	return ip != nil &&
+		(ip[0] == 10 || ip[0] == 172 && (ip[1] >= 16 && ip[1] < 32) || ip[0] == 192 && ip[1] == 168 || ip[0] == 169 && ip[1] == 254)
+}
+
+func lower16BitPrivateIP(interfaceAddrs InterfaceAddrs) (uint16, error) {
+	ip, err := privateIPv4(interfaceAddrs)
+	if err != nil {
+		return 0, err
+	}
+
+	return uint16(ip[2])<<8 + uint16(ip[3]), nil
+}
+
+// ElapsedTime returns the elapsed time when the given Sonyflake ID was generated.
+func ElapsedTime(id uint64) time.Duration {
+	return time.Duration(elapsedTime(id) * sonyflakeTimeUnit)
+}
+
+func elapsedTime(id uint64) uint64 {
+	return id >> (BitLenSequence + BitLenMachineID)
+}
+
+// SequenceNumber returns the sequence number of a Sonyflake ID.
+func SequenceNumber(id uint64) uint64 {
+	const maskSequence = uint64((1<<BitLenSequence - 1) << BitLenMachineID)
+	return id & maskSequence >> BitLenMachineID
+}
+
+// MachineID returns the machine ID of a Sonyflake ID.
+func MachineID(id uint64) uint64 {
+	const maskMachineID = uint64(1<<BitLenMachineID - 1)
+	return id & maskMachineID
+}
+
+// Decompose returns a set of Sonyflake ID parts.
+func Decompose(id uint64) map[string]uint64 {
+	msb := id >> 63
+	time := elapsedTime(id)
+	sequence := SequenceNumber(id)
+	machineID := MachineID(id)
+	return map[string]uint64{
+		"id":         id,
+		"msb":        msb,
+		"time":       time,
+		"sequence":   sequence,
+		"machine-id": machineID,
+	}
 }
