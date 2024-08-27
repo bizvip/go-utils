39d680b7b9c98bf539ca8c07415036bb097973a4
geek_inside <38232156+bizvip@users.noreply.github.com>
2024-08-28 02:37:43 +0700

    update new package

--- snowflake/quick.go
+++ snowflake/quick.go
@@ -6,8 +6,6 @@ package snowflake
 
 import (
 	"time"
-
-	"github.com/prometheus/common/log"
 )
 
 var f1, f2 *Sonyflake
@@ -19,24 +17,16 @@ func init() {
 		return 1, nil
 	}})
 	f2 = NewSonyflake(Settings{StartTime: startTime, MachineID: func() (uint16, error) {
-		return 1, nil
+		return 2, nil
 	}})
 }
 
 func QuickID() uint64 {
-	id, err := f1.NextID()
-	if err != nil {
-		log.Fatalf("Error getting ID: %s", err)
-		return 0
-	}
+	id, _ := f1.NextID() // 分析算法，基本不可能出错
 	return id
 }
 
 func QuickID2() uint64 {
-	id, err := f2.NextID()
-	if err != nil {
-		log.Errorf("Error getting ID: %s", err)
-
-	}
+	id, _ := f2.NextID()
 	return id
 }
