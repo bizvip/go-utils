303e96310e99364083efe11c807a08ec4acb10a2
geek_inside <38232156+bizvip@users.noreply.github.com>
2025-03-26 23:57:22 +0700

    updated codes

--- base/str/base26/base26.go
+++ base/str/base26/base26.go
@@ -1,9 +1,9 @@
 /******************************************************************************
- * Copyright (c) 2025. Archer++. All rights reserved.                         *
+ * Copyright (c) 2024. Archer++. All rights reserved.                         *
  * Author ORCID: https://orcid.org/0009-0003-8150-367X                        *
  ******************************************************************************/
 
-package str
+package base26
 
 import (
 	"strings"
